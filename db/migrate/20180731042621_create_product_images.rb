69bf113bdcf8fb6014823be1ee4e25128cf9630a
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-31 16:34:20 +0900

    create_product_image.rb_edit

--- db/migrate/20180731042621_create_product_images.rb
+++ db/migrate/20180731042621_create_product_images.rb
@@ -1,7 +1,9 @@
 class CreateProductImages < ActiveRecord::Migration
   def change
     create_table :product_images do |t|
-
+      t.image :string, null: false
+      t.status :integer, null: false
+      t.product_id :references, null: false, foreign_key: true
       t.timestamps null: false
     end
   end
