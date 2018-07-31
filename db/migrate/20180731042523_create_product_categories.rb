d5f52c647c282f8d919addf3d2e383b38a9f3b3a
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-31 16:22:11 +0900

    create_product_categories.rb_edit

--- db/migrate/20180731042523_create_product_categories.rb
+++ db/migrate/20180731042523_create_product_categories.rb
@@ -1,7 +1,10 @@
 class CreateProductCategories < ActiveRecord::Migration
   def change
     create_table :product_categories do |t|
-
+      t.product_id :references, null: false, foreign_key: true
+      t.large_class_id :references, null: false, foreign_key: true
+      t.middle_class_id :references, null: false, foreign_key: true
+      t.small_class_id :references, null: false, foreign_key: true
       t.timestamps null: false
     end
   end
