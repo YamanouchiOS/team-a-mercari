c3d8eb5ec3317c1ca547e1a339531f2c1b4a9b5e
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-31 17:34:30 +0900

    create_middle_classes.rb_edit

--- db/migrate/20180731042541_create_middle_classes.rb
+++ db/migrate/20180731042541_create_middle_classes.rb
@@ -1,9 +1,10 @@
 class CreateMiddleClasses < ActiveRecord::Migration
   def change
     create_table :middle_classes do |t|
-      t.name :string, null: false, unique: true
-      t.large_class_id :references, null: false, foreign_key: true
+      t.string :name, null: false, unique: true
+      t.references :large_class_id, null: false, foreign_key: true
       t.timestamps null: false
     end
   end
 end
+
