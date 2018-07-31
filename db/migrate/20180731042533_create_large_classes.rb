3c5e3efa6036aed86c83b03eecebd779c840feb0
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-31 16:23:55 +0900

    create_large_classes.rb_edit

--- db/migrate/20180731042533_create_large_classes.rb
+++ db/migrate/20180731042533_create_large_classes.rb
@@ -1,7 +1,7 @@
 class CreateLargeClasses < ActiveRecord::Migration
   def change
     create_table :large_classes do |t|
-
+      t.name :string, null: false, unique: true
       t.timestamps null: false
     end
   end
