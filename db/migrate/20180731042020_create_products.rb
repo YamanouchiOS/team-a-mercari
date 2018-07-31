cf19411b132870b97ff882de976d82f8b010ac85
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-31 16:11:40 +0900

    create_product.rb_edit
    ※condition, delivery_cost, prefecture, delivery_date,
    statusをenum型でモデルに定義する必要がある。

--- db/migrate/20180731042020_create_products.rb
+++ db/migrate/20180731042020_create_products.rb
@@ -1,8 +1,19 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.string :name, null: false, add_index: true
+      t.detail :text, null: false
+      t.price :unsigned_integer, null: false
+      t.user_id :references, null: false, foreign_key: true
+      t.condition :integer, null: false
+      t.delivery_cost :integer, null: false
+      t.prefecture :integer, null: false
+      t.delivery_date :integer, null: false
+      t.status :integer, null: false
+      t.likes_count :integer, default: 0
       t.timestamps null: false
     end
   end
 end
+
+# condition, delivery_cost, prefecture, delivery_date, statusは enum型をモデルで定義
