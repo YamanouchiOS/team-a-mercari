65f566903a6e6021ab687e7af2ae7db6ad27fed5
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-31 16:31:32 +0900

    create_small_classes.rb_edit

--- db/migrate/20180731042548_create_small_classes.rb
+++ db/migrate/20180731042548_create_small_classes.rb
@@ -1,7 +1,8 @@
 class CreateSmallClasses < ActiveRecord::Migration
   def change
     create_table :small_classes do |t|
-
+      t.name :string, null: false, unique: true
+      t.large_class_id :references, null: false, foreign_key: true
       t.timestamps null: false
     end
   end
