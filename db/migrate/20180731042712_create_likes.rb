0cc4a2245fdb71c7f78c0e5a02f488a9a75f2b40
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-31 17:39:16 +0900

    create_likes.rb_edit

--- db/migrate/20180731042712_create_likes.rb
+++ db/migrate/20180731042712_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
-      t.user_id :references, null: false, foreign_key: true
-      t.product_id :references, null: false, foreign_key: true
+      t.references :user_id, null: false, foreign_key: true
+      t.references :product_id, null: false, foreign_key: true
       t.timestamps null: false
     end
   end
