890d8188aa5665214d3c2f04da1548837e7da984
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-31 16:36:12 +0900

    create_comments.rb_edit

--- db/migrate/20180731042627_create_comments.rb
+++ db/migrate/20180731042627_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-
+      t.text :text, null: false
+      t.user_id :references, null: false, foreign_key: true
+      t.product_id :references, null: false, foreign_key: true
       t.timestamps null: false
     end
   end
