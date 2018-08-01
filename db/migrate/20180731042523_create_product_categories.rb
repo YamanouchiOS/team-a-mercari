91a7446c5c8502c990337f26fec712cd694bccb7
IshinShibata <shibataishin@shibata-Mac.local>
2018-08-01 12:43:10 +0900

    references型に対する_idを削除

--- db/migrate/20180731042523_create_product_categories.rb
+++ db/migrate/20180731042523_create_product_categories.rb
@@ -1,10 +1,10 @@
 class CreateProductCategories < ActiveRecord::Migration
   def change
     create_table :product_categories do |t|
-      t.references :product_id, null: false, foreign_key: true
-      t.references :large_class_id, null: false, foreign_key: true
-      t.references :middle_class_id, null: false, foreign_key: true
-      t.references :small_class_id, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.references :large_class, null: false, foreign_key: true
+      t.references :middle_class, null: false, foreign_key: true
+      t.references :small_class, null: false, foreign_key: true
       t.timestamps null: false
     end
   end
--- db/migrate/20180731042541_create_middle_classes.rb
+++ db/migrate/20180731042541_create_middle_classes.rb
@@ -2,7 +2,7 @@ class CreateMiddleClasses < ActiveRecord::Migration
   def change
     create_table :middle_classes do |t|
       t.string :name, null: false, unique: true
-      t.references :large_class_id, null: false, foreign_key: true
+      t.references :large_class, null: false, foreign_key: true
       t.timestamps null: false
     end
   end
