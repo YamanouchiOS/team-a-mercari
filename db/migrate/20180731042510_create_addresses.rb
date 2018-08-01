2d45e6cdfa7a9e8b22a97c1d5e9358bf8b1af3ae
IshinShibata <shibataishin@shibata-Mac.local>
2018-08-01 12:41:04 +0900

    user_idの_idを削除

--- db/migrate/20180731042510_create_addresses.rb
+++ db/migrate/20180731042510_create_addresses.rb
@@ -1,7 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
-      t.references :user_id, unique: true, null: false, foreign_key: true
+      t.references :user, unique: true, null: false, foreign_key: true
       t.integer :postal_code, null: false, add_index: true
       t.integer :prefecture,null: false
       t.string :city, null: false
