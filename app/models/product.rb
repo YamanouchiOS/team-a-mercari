09b9e4ded68a190519f278af97a4d28883986dfc
IshinShibata <shibataishin@shibata-Mac.local>
2018-08-01 12:38:43 +0900

    product.rb_edit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+    belongs_to :user, counter_cache: :products_count
+    has_one :product_category, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
+    has_many :product_images, dependent: :destroy
 end
