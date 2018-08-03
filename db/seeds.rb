7a3a317fbfa750ba827b0b772ffe3945f736e31f
Miho Kodama <k.mihob1028@gmail.com>
2018-08-03 10:22:33 +0900

    fixed class name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 require "csv"
 
 CSV.foreach('db/csv/large_class.csv') do |row|
-  Large_class.create(name: row[0], created_at: Time.zone.now, updated_at: Time.zone.now)
+  LargeClass.create(name: row[0], created_at: Time.zone.now, updated_at: Time.zone.now)
 end
 
 CSV.foreach('db/csv/middle_class.csv') do |row|
-  Middle_class.create(name: row[0], large_class_id: row[1], created_at: Time.zone.now, updated_at: Time.zone.now)
+  MiddleClass.create(name: row[0], large_class_id: row[1], created_at: Time.zone.now, updated_at: Time.zone.now)
 end
 
 CSV.foreach('db/csv/small_class.csv') do |row|
-  Small_class.create(name: row[0], middle_class_id: row[1],created_at: Time.zone.now, updated_at: Time.zone.now)
+  SmallClass.create(name: row[0], middle_class_id: row[1],created_at: Time.zone.now, updated_at: Time.zone.now)
 end
