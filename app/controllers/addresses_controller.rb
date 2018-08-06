8a9cb413e55d3743dc20c089e8d33699042529f4
Miho Kodama <k.mihob1028@gmail.com>
2018-08-06 17:55:08 +0900

    add save new address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,6 +5,14 @@ class AddressesController < ApplicationController
   end
 
   def create
+
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
   end
 
   def edit
@@ -14,6 +22,12 @@ class AddressesController < ApplicationController
   end
 
   def show
+  end
+
+private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :prefecture, :city, :street_number, :building_number, :phone_number, :family_name, :first_name, :family_kana, :first_kana, :introduction).merge(user_id: current_user.id)
   end
 
 end
