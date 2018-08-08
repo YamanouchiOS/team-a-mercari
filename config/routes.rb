9f2e116d2e2aceb81c96be9d4cf284529dd68ad1
Miho Kodama <k.mihob1028@gmail.com>
2018-08-08 22:54:34 +0900

    except index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'products#index'
   resources :products, only: [:index, :new]
-  resources :addresses, except: :destroy
+  resources :addresses, except: [:index, :destroy]
 end
