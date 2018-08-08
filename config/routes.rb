35f6d9376b63e241e60902c2780040e613b669b9
IshinShibata <shibataishin@shibata-Mac.local>
2018-08-08 15:38:15 +0900

    routes_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :products, only: [:index, :new]
+  resources :users, only: [:index, :new, :show] do
+  resources :products, only: [:index, :new, :show]
+  end
 end
