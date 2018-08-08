0cbdd0541afbe3606df0f4ee037b1c1f34f41e47
IshinShibata <shibataishin@shibata-Mac.local>
2018-08-08 15:36:51 +0900

    products_controlloer_edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,13 @@
 class ProductsController < ApplicationController
   def index
-
+    @products =Product.find(1)
   end
 
   def new
     @large_classes = LargeClass.all
+  end
+
+  def show
+    @product = Product.find_by(1)
   end
 end
