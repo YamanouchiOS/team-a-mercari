8f86e58d294c18a47e28f26e71a3d5c312993704
IshinShibata <shibataishin@shibata-Mac.local>
2018-08-08 21:20:08 +0900

    routes.rb_edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'products#index'
 
-  resources :users, only: [:index, :new, :show] do
+  resources :users, only: [:index, :new, :show]
   resources :products, only: [:index, :new, :create, :show]
-  end
-
   resources :addresses, except: [:index, :destroy]
 end
