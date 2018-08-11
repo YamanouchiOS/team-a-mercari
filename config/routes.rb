eed0bdac905e07acf22c3b1f296ffc55f153246b
Miho Kodama <k.mihob1028@gmail.com>
2018-08-11 17:34:32 +0900

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'products#index'
   resources :products, only: [:index, :new]
-  resources :addresses, except: [:index, :destroy]
+  resources :addresses, except: [:index, :destroy, :show]
+  resources :user, only: [:edit, :update, :show]
 end
