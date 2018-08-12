68a6f58d834984ebd15e66c13fa1a50daf09d6d9
Haya4Taka <takahiro@hayashitakahironoMacBook-ea.local>
2018-08-12 13:07:30 +0900

    Edit_Controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,13 @@ class CategoriesController < ApplicationController
   end
 
   def show
-  end
+    category_class = params[:id][0].to_i
+    category_id = params[:id][1,10].to_i
+    each_cat = [@large_classes = LargeClass.pluck(:id, :name), @middle_classes = MiddleClass.pluck(:id, :name, :large_class_id), @small_classes = SmallClass.pluck(:id, :name, :middle_class_id)]
+    @category_info = each_cat[category_class-1][category_id-1]  <<  category_class
 
+    @products = Product.fetch_products_of_category(category_class, category_id)
+
+    render :layout => "category"
+  end
 end
