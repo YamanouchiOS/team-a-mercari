b4c5e89231f0bee3c739071cdbd4549f2a02ff9b
Haya4Taka <takahiro@hayashitakahironoMacBook-ea.local>
2018-08-15 20:08:40 +0900

    Add_variables

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,7 @@ class OrdersController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
     @buyer = User.includes(:address).find(@product.buyer.id)
+    @seller = User.find(@product.user_id)
+    @user = User.find(current_user)
   end
 end
