913d5df029368fbcd4d9ad281a1ca04455c19f93
Haya4Taka <takahiro@hayashitakahironoMacBook-ea.local>
2018-08-15 20:08:59 +0900

    Define_helper_method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,22 @@ module ApplicationHelper
     SmallClass.pluck(:id, :name, :middle_class_id)
   end
 
+  def price_delimiter(price)
+    number_with_delimiter(price)
+  end
+
+  def define_order_status(product)
+    order_status_list = ["on_sale", "before_shipping", "before_receive-and-review", "before_sellers-review", "complete" ]
+    order_status = order_status_list[product.status]
+    return order_status
+  end
+
+  def buyer_or_seller(product, user)
+    if product.user_id == user.id
+      this_person = "seller"
+    elsif product.buyer_id == user.id
+      this_person = "buyer"
+    end
+  end
+
 end
