fdcc65d7b62a1e1e9c7f2d3a42823eb69eafd064
Haya4Taka <takahiro@hayashiacBookea.lan>
2018-08-17 01:27:24 +0900

    Dynamic_category_select

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -87,5 +87,82 @@ $(function(){
     reader.readAsDataURL(file);
   });
 
+
+
+
+
+
+  // カテゴリーの非同期表示
+
+  // 変数
+  var select_box_middle = `<select class="select-default" id="select_middle" name="product[product_category_attributes][middle_class_id"])></select>`
+  var select_box_small = `<select class="select-default" id="select_small" name="product[product_category_attributes][small_class_id"])></select>`
+  var default_select = `<option value=0>---</option>`
+
+  // 関数
+  function create_option_midddle(data){
+    var option = `<option value=${data.id }">
+  ${data.name}</option>`
+    $("#select_middle").append(option)
+  };
+
+  function create_option_small(data){
+    var option = `<option value=${data.id }">
+  ${data.name}</option>`
+    $("#select_small").append(option)
+  };
+
+  // イベント処理
+  $('#select_large').on("change", function(e) {
+    var input = $("#select_large").val();
+    var url = "/dynamic_select_middle";
+    console.log(input);
+    console.log(input + url);
+    $.ajax({
+      url: input + url,
+      type: "GET",
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      console.log(data);
+      console.log(select_box_middle)
+      $("#select_large").after(select_box_middle);
+      $("#select_middle").append(default_select);
+      data.forEach(function(data){
+        create_option_midddle(data)
+      })
+    })
+    .fail(function(){
+      alert("非同期通信に失敗しました")
+    })
+  })
+
+  $(".select-wrap").on("change", "#select_middle", function(e) {
+    var input = $("#select_middle").val();
+    var url = "/dynamic_select_small";
+    console.log(input);
+    console.log(input + url);
+    $.ajax({
+      url: input + url,
+      type: "GET",
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      console.log(data);
+      console.log(select_box_small)
+      $("#select_middle").after(select_box_small);
+      $("#select_small").append(default_select);
+      data.forEach(function(data){
+        create_option_small(data)
+      })
+    })
+    .fail(function(){
+      alert("非同期通信に失敗しました")
+    })
+  })
 });
 
---
+++ app/views/products/dynamic_select_small.json.jbuilder
@@ -0,0 +1,4 @@
+json.array! @small_classes do |small_class|
+  json.id small_class.id
+  json.name small_class.name
+end
